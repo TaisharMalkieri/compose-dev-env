5334940406e2dc5f58f81f26bea708e665f7c83c
Tormod <tormod_tho@hotmail.com>
2022-11-06 23:47:50 +0000

    asa

---
+++ backend/shell/utilities.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+GIT_USER="tormod"
+GIT_EMAIL="tormod_tho@hotmail.com"
+
+apt-get update                  \
+&& apt-get install -y git       \
+&& apt-get install -y curl      \
+&& apt-get install -y wget      \
+&& apt-get install -y unzip     \
+&& apt-get install -y tree      \
+&& apt-get update               \
+&& apt upgrade -y               \
+&& apt-get clean
+
+git config --global user.name "${GIT_USER}"
+git config --global user.email "${GIT_EMAIL}"
+
+git config --global --replace-all alias.am "!git commit --ammend --no-edit"
+git config --global --replace-all alias.save "!git add -A && git commit -m 'chore: commit save point'"
+git config --global --replace-all alias.save "!git rebase -i"
